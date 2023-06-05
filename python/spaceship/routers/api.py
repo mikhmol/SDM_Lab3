61393250e27a7479cd8a1d582bc55a878f835fc5
mikhmol <mikhailo.moliakov@gmail.com>
2023-06-05 19:45:30 +0300

    Add matrix endpoint

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {'msg': 'Hello, World! My name is Mikhailo Moliakov'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
+    	
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
